7878e0594758818f7b2c636cea0200f2b5dbc45c
LOUIS <30631438+mothwal@users.noreply.github.com>
2020-06-23 13:14:13 +0200

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ###### This is the basical code of  Coursera_Capstone Project to print a statement.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
